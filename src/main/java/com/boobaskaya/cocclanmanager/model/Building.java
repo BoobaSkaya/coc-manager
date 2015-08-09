956a9fe97169ca3327c7659b3a5de424a4921bf8
Booba Skaya <booba.skaya@gmail.com>
2015-08-09 07:30:27 +0200

    Display hitpoints of buildings in the GUI

--- src/main/java/com/boobaskaya/cocclanmanager/model/Building.java
+++ src/main/java/com/boobaskaya/cocclanmanager/model/Building.java
@@ -4,7 +4,6 @@ package com.boobaskaya.cocclanmanager.model;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlSeeAlso;
 
-import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -12,12 +11,14 @@ import javafx.beans.property.SimpleStringProperty;
 @XmlSeeAlso({Cannon.class, GoldMine.class, Hdv.class})
 public abstract class Building  {  
     private SimpleIntegerProperty level;
+	private SimpleIntegerProperty hitpoints;
     public Building(){
         this(1);
     }
     
     public Building(int level){
-        this.level  = new ReadOnlyIntegerWrapper(level);
+		this.level = new SimpleIntegerProperty(level);
+		this.hitpoints = new SimpleIntegerProperty(getConfig().getHitPoints(level));
     }
     
     @XmlAttribute(name = "level")
@@ -27,12 +28,19 @@ public abstract class Building  {
     
     public void setLevel(int level){
         this.level.set(level);
+		this.hitpoints.setValue(getConfig().getHitPoints(level));
     }
     
     public ReadOnlyStringProperty typeProperty(){
         return new SimpleStringProperty(getType().toString());
     }
+
+	public SimpleIntegerProperty hitpointsProperty() {
+		return this.hitpoints;
+	}
+
     public abstract BuildingType getType();
+
     public abstract int getMaxLevel(Hdv hdv);
 
 	public abstract Config getConfig();
